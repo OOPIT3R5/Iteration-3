3da60b1e1d329ee086d11f863a53cb81704822fc
Jeff G <drak@ufl.edu>
2015-04-17 11:59:24 -0400

    Worked on statistics, added basic methods and a public interface.

--- OOP-Iteration-3/src/Model/Entity/Statistic.java
+++ OOP-Iteration-3/src/Model/Entity/Statistic.java
@@ -1,4 +1,60 @@
 package Model.Entity;
 
 public class Statistic {
+
+    private int minimum;
+    private int maximum;
+    private int currentvalue;
+    private int temporaryvalue;
+    private int levelupboost;
+
+    public Statistic(){
+        minimum = 0;
+        maximum = Integer.MAX_VALUE;
+        currentvalue = 0;
+        levelupboost = 1;
+    }
+
+    public Statistic(int minimum, int maximum){
+        this.minimum = minimum;
+        this.maximum = maximum;
+        currentvalue = minimum;//Set it up to be the lowest
+        levelupboost = 1;
+    }
+
+    public Statistic(int minimum, int maximum, int levelupboost){
+        this.minimum = minimum;
+        this.maximum = maximum;
+        currentvalue = minimum;
+        this.levelupboost = levelupboost;
+    }
+
+    public int getCurrentValue(){
+        if(currentvalue + temporaryvalue > maximum)
+            return maximum;
+        else return (currentvalue + temporaryvalue);
+    }
+
+    //PRECONDITION: This parameter is only passed once for the entirety of the temporary boost.
+    public void addTemporaryValue(int tempval){
+        temporaryvalue += tempval;
+    }
+
+    /* REMOVE? Reason: Replicated behavior.Could just pass in negative value to addTemporaryValue()
+    //PRECONDITION: The same parameter tempval must have been passed to the void addTemporaryValue() method earlier.
+    public void removeTemporaryValue(int tempval){
+        temporaryvalue -= tempval;
+    } */
+
+    public void addPermanentValue(int val){
+        currentvalue += val;
+        if (currentvalue > maximum)
+            currentvalue = maximum;
+        if (currentvalue < minimum)
+            currentvalue = minimum;
+    }
+
+    public void levelUpStat(){
+        addPermanentValue(levelupboost);
+    }
 }
--- OOP-Iteration-3/src/Model/Entity/StatisticContainer.java
+++ OOP-Iteration-3/src/Model/Entity/StatisticContainer.java
@@ -1,5 +1,65 @@
 package Model.Entity;
 
 public class StatisticContainer {
+    private Statistic strength;
+    private Statistic agility;
+    private Statistic intellect;
+    private Statistic hardiness;
+    private Statistic experience;
+    private Statistic movement;
+    private Statistic level;
 
+    public StatisticContainer(){
+        strength = new Statistic();
+        agility = new Statistic();
+        intellect = new Statistic();
+        hardiness = new Statistic();
+        experience = new Statistic();
+        movement = new Statistic(1,50);
+        level = new Statistic(1,100,0);
+    }
+
+    public int getLevel(){
+        return level.getCurrentValue();
+    }
+
+    public int getStrength() {
+        return strength.getCurrentValue();
+    }
+
+    public int getAgility(){
+        return agility.getCurrentValue();
+    }
+
+    public int getIntellect(){
+        return intellect.getCurrentValue();
+    }
+
+    public int getHardiness(){
+        return hardiness.getCurrentValue();
+    }
+
+    public int getMovement(){
+        return movement.getCurrentValue();
+    }
+
+    private void checkLevelUp(){
+        if(level.getCurrentValue() < (experience.getCurrentValue() / 1000)){
+            level.addPermanentValue(1);             //Update level.
+            levelupAllStatistics();                 //Level up all primary statistics.
+        }
+    }
+
+    public void awardExperience(int experience){
+        this.experience.addPermanentValue(experience);  // Add the experience to the container.
+        checkLevelUp();                                 // Check if we had a level up after this.
+    }
+
+    private void levelupAllStatistics(){
+        strength.levelUpStat();
+        agility.levelUpStat();
+        intellect.levelUpStat();
+        hardiness.levelUpStat();
+        movement.levelUpStat();
+    }
 }
