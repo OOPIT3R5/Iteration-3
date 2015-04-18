ef88082243d6578ce1e9e23cb25a43a12a9e7416
Phillip Riley <phillipriley@ufl.edu>
2015-04-18 19:11:24 -0400

    Changes to Ability classes.

--- OOP-Iteration-3/src/Model/Entity/Ability/MultipleTargetAbility.java
+++ OOP-Iteration-3/src/Model/Entity/Ability/MultipleTargetAbility.java
@@ -5,5 +5,5 @@ import java.util.ArrayList;
 import Model.Map.Grid.Tile.Tile;
 
 public abstract class MultipleTargetAbility extends SkillAbility{
-	protected abstract ArrayList<Tile> getTargetTiles();
+	protected abstract ArrayList<Tile> getTargetTiles();		// should be map instead of tiles
 }
