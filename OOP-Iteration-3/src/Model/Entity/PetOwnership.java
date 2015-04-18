6a8145a4a08fe931f41793c70b61823bd21bc2bb
jthakore <jthakore1@gmail.com>
2015-04-18 00:43:56 -0400

    Pet Stuff

--- OOP-Iteration-3/src/Model/Entity/PetOwnership.java
+++ OOP-Iteration-3/src/Model/Entity/PetOwnership.java
@@ -1,8 +1,21 @@
 package Model.Entity;
 
+import Model.Entity.Ability.Move;
+
 public class PetOwnership {
 
 	public PetOwnership(){
 		
+	}
+
+	public void setPet(TroublesomeMenace troublesomeMenace) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	public Move followAvatar() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 }
--- OOP-Iteration-3/src/Model/Entity/TroublesomeMenace.java
+++ OOP-Iteration-3/src/Model/Entity/TroublesomeMenace.java
@@ -1,9 +1,58 @@
 package Model.Entity;
 
-public class TroublesomeMenace extends NonAdversarial{
+import Model.Entity.Ability.*;
+import Model.Map.Direction;
+import Utility.RandomGenerator;
 
-	public TroublesomeMenace() {
-		// TODO Auto-generated constructor stub
+public class TroublesomeMenace extends NonAdversarial implements Pet{
+	
+	boolean owned;
+	PetOwnership po;
+	
+	public TroublesomeMenace(PetOwnership po) {
+		super();
+		po.setPet(this);
+		this.po = po;
+	}
+	
+	/*AI stuff*/
+	public void makeActionChoice(){
+		Ability a;
+		/*will never be hostile; will always either follow avatar (if owned),
+		attack in vicinity, or go after treasure*/
+		RandomGenerator randomlyGenerate = new RandomGenerator();
+		double rand = randomlyGenerate.probabiltiy();
+		Direction randDir = randomlyGenerate.direction();
+		
+		if(rand <.65){
+			//65% chance of following (behind) avatar
+			a = po.followAvatar();
+		}
+		else if(rand<.75){
+			//10% chance of attacking in a random direction
+			a = attackInVicinity();//change this so that it returns an ability
+		}
+		else if(rand <.95){
+			//20% chance of running around in random direction in attempt to pick up item
+			a = stealInVicinity();
+		}
+		else{
+			//5% chance of doing nothing
+			a = new DoNothing();
+		}
+		a.execute();
+	}
+	@Override
+	public Ability attackInVicinity() {
+		return new Attack(null, this);
+		//find closest entity (not avatar), face, and attack
+	}
+
+	@Override
+	public Ability stealInVicinity() {
+		return new Move(this, getDirectionFacing(), getMovementSpeed());
+		//can pickup item on tile
+		//will race ahead of avatar(upto 2 tiles) in direction avatar is facing		
 	}
 
 }
