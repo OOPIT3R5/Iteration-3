b3bc125eae6aa93c85240b1d6312f0b87acfde83
Phillip Riley <phillipriley@ufl.edu>
2015-04-18 15:05:17 -0400

    Trying to merge files.

--- OOP-Iteration-3/src/Model/Entity/Ability/Ability.java
+++ OOP-Iteration-3/src/Model/Entity/Ability/Ability.java
@@ -1,9 +1,5 @@
 package Model.Entity.Ability;
 
-import java.util.ArrayList;
-
-import Model.Entity.Entity;
-
 public abstract class Ability {
     public abstract void execute();
 }
--- OOP-Iteration-3/src/Model/Entity/Ability/Move.java
+++ OOP-Iteration-3/src/Model/Entity/Ability/Move.java
@@ -1,29 +1,26 @@
 package Model.Entity.Ability;
 
-import java.util.ArrayList;
-
 import Model.Entity.Entity;
 import Model.Map.Direction;
-import Model.Map.Grid.Tile.Tile;
 
 public class Move extends Ability {
 	
 	private int movementSpeed;
 	private Direction direction;
-    private Entity e;
+    private Entity entity;
 
-	public Move(Entity e, Direction direction, int movementSpeed) {
+	public Move(Entity entity, Direction direction, int movementSpeed) {
 		this.direction = direction;
 		this.movementSpeed = movementSpeed;
-	    this.e = e;
+	    this.entity = entity;
     }
 
 	@Override
 	public void execute() {
-		Entity currentEntity = e;
+		Entity currentEntity = entity;
 		// get oldTile
 		System.out.println(currentEntity.getLocation().toString());
-		currentEntity.setLocation(currentEntity.getLocation().getNeighbor(direction));
+		currentEntity.setLocation(currentEntity.getLocation().getNeighbor(direction));		// incorporate movement speed?
 		System.out.println(currentEntity.getLocation().toString());
 		// get newTile
 		// remove Entity from oldTile
