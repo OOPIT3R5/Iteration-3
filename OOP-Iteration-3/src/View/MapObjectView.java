dc0ecfa3b5a829cd9fd02604661a576cbfbbb6d1
Jeff G <drak@ufl.edu>
2015-04-18 23:54:44 -0400

    Added avatar/entity possibility sprites and new static method for making a BufferedImage out of them.

--- OOP-Iteration-3/src/View/MapObjectView.java
+++ OOP-Iteration-3/src/View/MapObjectView.java
@@ -23,4 +23,8 @@ public class MapObjectView {
     public static BufferedImage getSpriteFromFE(int col, int row) throws IOException {
         return ImageIO.read(new File(System.getProperty("user.dir") + "/OOP-Iteration-3/Assets/weapon_sprites.png")).getSubimage(24*col,24*row,24,24);
     }
+
+    public static BufferedImage getSpriteFromAvatar(int col, int row) throws IOException {
+        return ImageIO.read(new File(System.getProperty("user.dir") + "/OOP-Iteration-3/Assets/weapon_sprites.png")).getSubimage(32*col,32*row,32,32);
+    }
 }
