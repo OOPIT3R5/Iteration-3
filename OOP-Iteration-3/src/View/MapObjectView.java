e39d55a71c3834a1908a2cd1242f3bb3389eb646
hanif <hanif314@gmail.com>
2015-04-19 00:50:23 -0400

    file paths fixed

--- OOP-Iteration-3/src/View/MapObjectView.java
+++ OOP-Iteration-3/src/View/MapObjectView.java
@@ -17,14 +17,14 @@ public class MapObjectView {
     }
 
     public static BufferedImage getSpriteFromPokemon(int col, int row) throws IOException {
-        return ImageIO.read(new File(System.getProperty("user.dir") + "/OOP-Iteration-3/Assets/item_sprites.png")).getSubimage(24*col,24*row,24,24);
+        return ImageIO.read(new File(System.getProperty("user.dir") + "/Assets/item_sprites.png")).getSubimage(24*col,24*row,24,24);
     }
 
     public static BufferedImage getSpriteFromFE(int col, int row) throws IOException {
-        return ImageIO.read(new File(System.getProperty("user.dir") + "/OOP-Iteration-3/Assets/weapon_sprites.png")).getSubimage(24*col,24*row,24,24);
+        return ImageIO.read(new File(System.getProperty("user.dir") + "/Assets/weapon_sprites.png")).getSubimage(24*col,24*row,24,24);
     }
 
     public static BufferedImage getSpriteFromAvatar(int col, int row) throws IOException {
-        return ImageIO.read(new File(System.getProperty("user.dir") + "/OOP-Iteration-3/Assets/entity_sprites.png")).getSubimage(32*col,32*row,32,32);
+        return ImageIO.read(new File(System.getProperty("user.dir") + "/Assets/entity_sprites.png")).getSubimage(32*col,32*row,32,32);
     }
 }
