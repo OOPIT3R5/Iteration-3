4105bf70da4ddebd62d8e44cfe527d9140e1bae5
jthakore <jthakore1@gmail.com>
2015-04-20 09:29:25 -0400

    AreaEffect More stuff

--- OOP-Iteration-3/src/Model/Map/AreaEffects/AreaEffect.java
+++ OOP-Iteration-3/src/Model/Map/AreaEffects/AreaEffect.java
@@ -1,13 +1,21 @@
 package Model.Map.AreaEffects;
 
+import java.awt.Graphics;
+
 import Model.Entity.Entity;
 import Model.Entity.Ability.Ability;
+import Model.Items.MapObject;
+import Model.Map.HexagonalLocation;
+import Model.Map.Location;
+import View.Model.MapObjectView;
 
-public class AreaEffect {
+public class AreaEffect extends MapObject{
 	private Ability ability;
-	
-	public AreaEffect(Ability ability){
+	private MapObjectView mov;
+	   
+	public AreaEffect(Ability ability, MapObjectView mov){
 		this.ability = ability;
+		this.mov = mov;
 	}
 	
 	public boolean isPassable(){
@@ -16,5 +24,22 @@ public class AreaEffect {
 	
 	public void onTouch(Entity e){
 		ability.execute();
+	}
+
+	@Override
+	public MapObjectView getMapObjectView() {
+		return mov;
+	}
+
+	@Override
+	protected void render(Graphics g, HexagonalLocation center,
+			Location location) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getName() {
+		return ability.toString();
 	}
 }
